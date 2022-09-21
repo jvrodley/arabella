4a33d39fc4417d59e3f93db95b73a2eab3967588
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-09-21 15:52:47 -0400

    dispatched an event to a repo using octokit - proves auth works

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This project was conceived in the wake of the Social Network Hackathon.  All the work on this project, investigation, documentation and coding is documented in the github repository at <link>.  The history of what work was done when can be seen there in its entirety.  As per hack rule and tradition, all coding was left for the period of the hack itself.  
 
 However, much of the prep-work including documentation and design was pre-loaded (see the github history).  If that negatively affects our hack score, c’est la guerre, we’re prepared to take the hit.
+
+Notes:
+On windows requires openssl 1.1.1 - 3.0.5 fails with unable to load shared library
---
+++ __tests__/test_dispatch_event_api.js
@@ -0,0 +1,58 @@
+
+const expect = import('chai').expect;
+const assert = import('chai').assert;
+
+import{ App } from "octokit";
+import fs from "fs";
+
+let my_token = process.env.MY_GITHUB_TOKEN
+let my_forked_project = process.env.FORKED_PROJECT
+let appId = process.env.APP_ID
+let private_key_file_name = process.env.PRIVATE_KEY_FILE_NAME
+let app_name = process.env.APP_NAME
+
+let tokenAuth // NOTE: not real token
+let client
+
+describe("FORK PROJECT", () => {
+    console.log("fork_project")
+    it('dispatch_event', async function () {
+        console.log("process.env.NODE_ENV = " + process.env.NODE_ENV)
+        await test_dispatchevent()
+    });
+
+    function authenticate() {
+        client = new GitHubClient(new ProductHeaderValue(app_name));
+        tokenAuth = new Credentials(my_token); // NOTE: not real token
+        client.Credentials = tokenAuth;
+    }
+
+    async function test_dispatchevent() {
+        let keybuffer
+        try {
+            keybuffer = fs.readFileSync(private_key_file_name, 'utf8');
+        } catch(e) {
+            console.log('Error:', e.stack);
+        }
+
+        let private_key = keybuffer.toString()
+        console.log("Instantiating app")
+        const app = new App({appId: appId, privateKey: private_key});
+
+        for await (const {octokit, repository} of app.eachRepository.iterator()) {
+            console.log("eventing repo named " + repository.name )
+            // https://docs.github.com/en/rest/reference/repos#create-a-repository-dispatch-event
+            await octokit.rest.repos.createDispatchEvent({
+                owner: repository.owner.login,
+                repo: repository.name,
+                event_type: "test_event",
+                client_payload: {
+                    foo: "bar",
+                },
+            });
+            console.log("Event dispatched for repo named " + repository.full_name);
+        }
+    }
+
+
+})
