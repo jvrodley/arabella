992b5d177f7a98f6d73edaee204d6bb93d4bfeae
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-09-23 23:54:46 -0400

    get needs from db rather than hardwired

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -2,7 +2,7 @@ export async function getNeeds(host, port) {
     console.log("getNeeds calling out to api")
 
     return new Promise( async (resolve, reject) => {
-        const response = await fetch('http://'+host+':'+port+'/needs');
+        const response = await fetch('https://'+host+':'+port+'/needs');
         if(response.ok) {
             let x = await response.json();
             console.log("Got needs " + JSON.stringify(x));
