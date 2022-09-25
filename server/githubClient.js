065c9181456febad400542160f62ff08f1e85996
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-09-25 00:56:59 -0400

    github webhooks

--- server/githubClient.js
+++ server/githubClient.js
@@ -3,7 +3,7 @@ import { Octokit } from "@octokit/rest";
 
 class GithubClient {
     constructor() {
-        this.instance = new Octokit({ auth: process.env.GITHUB_TOKEN });
+        this.instance = new Octokit({ auth: process.env.MY_GITHUB_TOKEN });
     }
 }
 export default GithubClient
