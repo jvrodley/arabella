ba574f07b973fdc3f355be94e7fdd67ac4685699
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-09-25 03:12:26 -0400

    github webhooks

--- server/discord_stuff.js
+++ server/discord_stuff.js
@@ -261,5 +261,6 @@ export async function sendMessageToChannel( channel_name, message ) {
     await client.login(process.env.DISCORD_TOKEN);
 
     let guild = await client.guilds.fetch(process.env.GUILD_ID)
-    const ret = guild.channels.cache.find(channel => channel.name === "needs").send("PR TO MAIN ACCEPTED!");
+    const channel = guild.channels.cache.find(channel => channel.name === "needs")
+        channel.send("PR TO MAIN ACCEPTED!");
 }
